1a6770a774261fd5d872bd9a158bafca99329d9e
FluxQQZZy18 <155860610+FluxQQZZy18@users.noreply.github.com>
2024-01-14 17:57:21 -0500

    Update annotate.py

--- pigeonXT/annotate.py
+++ pigeonXT/annotate.py
@@ -289,8 +289,8 @@ def annotate(
             annotations.at[ix, value_column] = annotation
         annotations.at[ix, "changed"] = True
         if example_process_fn is not None:
-            print(current_index)
-            example_process_fn(annotations.at[ix, example_column], annotation, ix)
+            print(annotations)
+            example_process_fn(annotations.at[ix, example_column], annotation, ix, annotations)
         next_example()
 
     def next_example(*_, **__):
